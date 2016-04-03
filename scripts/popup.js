972b0dddf23cb4524f4963c07ba8747084f1bddb
BrianH2 <brianhemeryck@gmail.com>
2016-04-03 09:41:49 -0700

    Show error if credentials are not saved

--- scripts/popup.js
+++ scripts/popup.js
@@ -36,8 +36,8 @@ function injectScript(){
 		document.getElementById("success").innerHTML = "Running on page now!";
 		document.getElementById("success").style.display = "block";
 	} else {
-		document.getElementById("error").innerHTML = "Please save your API keys in the <a href='options.html'>options</a> first";
-		document.getElementById("success").style.display = "block";
+		document.getElementById("error").innerHTML = "You must save your API keys in the <a href='options.html'>options</a> before this will work";
+		document.getElementById("error").style.display = "block";
 	}
 }
 
